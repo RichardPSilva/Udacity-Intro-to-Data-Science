44c302a832b6043d86d66562915af58b4219688e
RichardPSilva <richardpisilva@gmail.com>
2019-01-06 16:44:37 -0200

    Programas em python do curso

---
+++ aula 1/00_numpytest.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy
+
+numbers = [1, 2, 3, 4, 5]
+print(numpy.mean(numbers)) #mostra a média do array numbers
+print(numpy.median(numbers)) #mostra a mediana do array numbers
+print(numpy.std(numbers)) #mostra o desvio padrão do array numbers
